b55029ded8e141df29fbce9636208558ee9b0dfa
daniel2196 <daniel2196@localhost>
2007-09-28 02:09:49 +0000

    Configuration management for SGE workers.

--- src/workerServer.py
+++ src/workerServer.py
@@ -4,23 +4,35 @@
 """
 workerServer - A frontend 'driver' to start a server instance
 
-
-
 """
 # Copyright (c) 2007 Daniel Wang
 # This file is part of SWAMP.
 # SWAMP is released under the GNU General Public License version 3 (GPLv3)
 
-import swamp.server
+import getopt
 import sys
+import swamp.server
+
 
 def test():
     swamp.server.pingServer("slave.conf")
     
 def main():
-    swamp.server.startServer("slave.conf")
+    confname = "slave.conf"
+    # Split this config handling to something scalable
+    # if once we have >2 options
+    (opts, leftover) = getopt.getopt(sys.argv[1:], "c:",["config="])
+    offeredConfs = []
+    for o in opts:
+        if (o[0] == "-c") or (o[0] == "--config"):
+            offeredConfs.append(o[1])
+    if offeredConfs:
+        confname = offeredConfs
+
+    swamp.server.startServer(confname)
 
 if __name__ == '__main__':
+    # degenerate test-mode
     if (len(sys.argv) > 1) and sys.argv[1] == "-t":
         test()
     else:
