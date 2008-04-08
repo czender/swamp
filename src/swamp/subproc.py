d561bd46f663ebe7169cf263e8c46e54f111d024
daniel2196 <daniel2196@localhost>
2008-04-08 21:45:50 +0000

    Added overrides for problematic twisted code

--- src/swamp/subproc.py
+++ src/swamp/subproc.py
@@ -35,6 +35,58 @@ def pythonCall(executable, args):
     code = proc.returncode
     return [code, output]
 
+
+
+# Re-define two functions that Twisted uses.
+from twisted.python import log as tLog
+from twisted.internet.process import unregisterReapProcessHandler
+import os
+def reapProcess(self):
+    """Try to reap a process (without blocking) via waitpid.
+    
+    This is called when sigchild is caught or a Process object loses its
+    "connection" (stdout is closed) This ought to result in reaping all
+    zombie processes, since it will be called twice as often as it needs
+    to be.
+
+    (Unfortunately, this is a slightly experimental approach, since
+    UNIX has no way to be really sure that your process is going to
+    go away w/o blocking.  I don't want to block.)
+
+    We replace Twisted's version with another that tolerates a
+    non-integer self.pid.
+    """
+    try:
+        pid, status = os.waitpid(self.pid, os.WNOHANG)
+    except:
+        tLog.msg('Failed to reap %s:' % str(self.pid)) # This line changed.
+        tLog.err()
+        pid = None
+    if pid:
+        self.processEnded(status)
+        unregisterReapProcessHandler(pid, self)
+
+from twisted.internet.process import reapProcessHandlers
+def registerReapProcessHandler(pid, process):
+    if reapProcessHandlers.has_key(pid):
+        raise RuntimeError
+    try:
+        aux_pid, status = os.waitpid(pid, os.WNOHANG)
+    except:
+        log.msg('Failed to reap %s:' % str(pid)) # This line changed.
+        log.err()
+        aux_pid = None
+    if aux_pid:
+        process.processEnded(status)
+    else:
+        reapProcessHandlers[pid] = process
+
+
+# Replace Twisted's code with one that doesn't crash (Python 2.4.2, amd64)
+import twisted.internet.process
+twisted.internet.process.Process.reapProcess = reapProcess
+twisted.internet.process.Process.registerReapProcessHandler = registerReapProcessHandler
+
 def mkCallback(code, result, sema):
     def callback(tup):
         out,err,code = tup
@@ -59,3 +111,4 @@ def twistedCall(executable, args):
     sema.acquire() # Block until it's ready.
     #print "returning result",result
     return result
+
